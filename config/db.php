29c1c8a74ccc8a1526166845f6d1d3781d1cd4d4
renat1015 <renat1015@bk.ru>
2026-03-24 15:50:01 +0300

    chore: update readme

--- config/db.php
+++ config/db.php
@@ -7,7 +7,7 @@ return [
     'dsn' => 'mysql:host=' . (getenv('DB_HOST') ?: '127.0.0.1') . 
             ';port=' . (getenv('DB_PORT') ?: '3306') . 
             ';dbname=' . (getenv('DB_NAME') ?: 'short_link_db'),
-    'username' => getenv('DB_USER') ?: 'user',
-    'password' => getenv('DB_PASSWORD') ?: 'password',
+    'username' => getenv('DB_USER') ?: 'your_user',
+    'password' => getenv('DB_PASSWORD') ?: 'your_password',
     'charset' => 'utf8mb4',
 ];
